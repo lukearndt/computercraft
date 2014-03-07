a6c1824d8c3a10c749a961a44b0813ff5df3df86
lukearndt <luke.j.arndt@gmail.com>
2014-03-08 01:46:46 +1300

    Split redstone timer into UI & logic and push logic into controller

--- RedstoneController.lua
+++ RedstoneController.lua
@@ -7,3 +7,26 @@ function redstone_off(side)
   print("Disabling redstone output on side: " .. side)
   redstone.setOutput(side, false)
 end
+
+local function emit_redstone_on_sides(side_count, sides)
+  for side = 1, side_count, 1 do
+    redstone_on(sides[side])
+  end
+end
+
+local function stop_redstone_on_sides(side_count, sides)
+  for side = 1, side_count, 1 do
+    redstone_off(sides[side])
+  end
+end
+
+function redstone_timer(emit_length, emit_delay, side_count, sides)
+  while true do
+    emit_redstone_on_sides(side_count, sides)
+    print("Waiting for " .. emit_length .. " seconds")
+    sleep(emit_length)
+    stop_redstone_on_sides(side_count, sides)
+    print("Waiting for " .. emit_delay .. " seconds")
+    sleep(emit_delay)
+  end
+end
--- RedstoneTimerUI.lua
+++ RedstoneTimerUI.lua
@@ -1,28 +1,5 @@
 load_package("redstone_controller")
 
-local function emit_redstone_on_sides(side_count, sides)
-  for side = 1, side_count, 1 do
-    redstone_on(sides[side])
-  end
-end
-
-local function stop_redstone_on_sides(side_count, sides)
-  for side = 1, side_count, 1 do
-    redstone_off(sides[side])
-  end
-end
-
-local function redstone_timer(emit_length, emit_delay, side_count, sides)
-  while true do
-    emit_redstone_on_sides(side_count, sides)
-    print("Waiting for " .. emit_length .. " seconds")
-    sleep(emit_length)
-    stop_redstone_on_sides(side_count, sides)
-    print("Waiting for " .. emit_delay .. " seconds")
-    sleep(emit_delay)
-  end
-end
-
 print("How long would you like to emit for each cycle?")
 local emit_length = tonumber(read())
 
