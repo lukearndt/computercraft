37c0fbc0efbbd16a1f65a0832c379cca4ea9318b
lukearndt <luke.j.arndt@gmail.com>
2014-03-08 01:47:03 +1300

    Create command-line redstone timer that uses redstone_controller

---
+++ RedstoneTimerCLI.lua
@@ -0,0 +1,18 @@
+args = { ... }
+
+load_package("redstone_controller")
+
+if args[1] == "" or args[2] == "" or args[3] == "" or args[4] == "" then
+  error("Insufficient arguments provided. Arguments required: emit_length, emit_delay, side_1 (, side_2, side_3, etc)")
+end
+
+local emit_length = tonumber(args[1])
+local emit_delay = tonumber(args[2])
+local side_count = tonumber(args[3])
+local sides = {}
+
+for side = 1, side_count, 1 do
+  sides[side] = args[side + 3]
+end
+
+redstone_timer(emit_length, emit_delay, side_count, sides)
